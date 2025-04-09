1bafac67b67b9ab49586c92625f55845a0d1ec46
victorhugo <victorhugolc.2611@gmail.com>
2025-04-09 19:20:28 -0300

    Att do Java Dio de UML

---
+++ src/IPhone.java
@@ -0,0 +1,50 @@
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    // ReprodutorMusical
+    @Override
+    public void tocar() {
+        System.out.println("Reproduzindo música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando a música: " + musica);
+    }
+
+    // AparelhoTelefonico
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada...");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    // NavegadorInternet
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Atualizando página...");
+    }
+}
